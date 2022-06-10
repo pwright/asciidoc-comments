f259ebfb12c3aa112c2bcbd5686d3ada922359a4
Paul Wright <pwright@redhat.com>
2022-06-10 14:28:01 +0100

    update docs

---
+++ README.md
@@ -0,0 +1,7 @@
+# Asciidoc comments
+
+Render asciidoc to html with each block addressable  for comments.
+
+```
+node ./index.js ./docs/test.adoc 
+```
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const asciidoctor = require('asciidoctor')()
 const registry = asciidoctor.Extensions.create()
 require('./add-id-processor.js')(registry)
 
-const doc = asciidoctor.convertFile('test.adoc', { 'extension_registry': registry })
-console.log(doc.getBlocks()[0].getSource()) // 'GDPR compliant :)'
+const doc = asciidoctor.convertFile(process.argv[1], { 'extension_registry': registry })
+console.log('File processed.')
