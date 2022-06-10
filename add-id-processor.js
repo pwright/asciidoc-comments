4a3c6b7c624058cebc42698e270877c7f0161770
Martin Murphy <mmurphy@redhat.com>
2022-06-10 14:11:31 +0100

    rename processor file add counter remove debugging console.log

--- add-id-processor.js
+++ add-id-processor.js
@@ -1,11 +1,11 @@
 var util = require('util')
 
+var counter = 0
 
 function processblocks(blocks) {
       for (var i =0; i<blocks.length; i++) {
-          console.log(util.inspect(blocks[i]))
           if(blocks[i].setId) {
-		blocks[i].setId("5");
+		blocks[i].setId("comment_"+counter++);
 	}
           if(blocks[i].hasBlocks && blocks[i].hasBlocks()){
 		processblocks(blocks[i].getBlocks())
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const asciidoctor = require('asciidoctor')()
 const registry = asciidoctor.Extensions.create()
-require('./gdpr-tree-processor.js')(registry)
+require('./add-id-processor.js')(registry)
 
 const doc = asciidoctor.convertFile('README.adoc', { 'extension_registry': registry })
 console.log(doc.getBlocks()[0].getSource()) // 'GDPR compliant :)'
