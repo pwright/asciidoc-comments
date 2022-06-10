a4444fbb50d22857b97560eae47fb51d28543174
Paul Wright <pwright@redhat.com>
2022-06-10 14:57:14 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
+
+const myArgs = process.argv.slice(2);
 const asciidoctor = require('asciidoctor')()
 const registry = asciidoctor.Extensions.create()
 require('./add-id-processor.js')(registry)
 
-const doc = asciidoctor.convertFile(process.argv[1], { 'extension_registry': registry })
+const doc = asciidoctor.convertFile(myArgs[0], { 'extension_registry': registry })
