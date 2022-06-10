04c9d580cbf26299e19e9cfbdc422346e3501915
Paul Wright <pwright@redhat.com>
2022-06-10 12:48:46 +0100

    update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const asciidoctor = require('asciidoctor')()
 const registry = asciidoctor.Extensions.create()
 require('./gdpr-tree-processor.js')(registry)
 
-const doc = asciidoctor.convertFile('sample-gdpr-doc.adoc', { 'extension_registry': registry })
+const doc = asciidoctor.convertFile('README.adoc', { 'extension_registry': registry })
 console.log(doc.getBlocks()[0].getSource()) // 'GDPR compliant :)'
