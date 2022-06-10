a33a20fc31375a336b362ef27dcc75ef329271f3
Martin Murphy <mmurphy@redhat.com>
2022-06-10 13:37:58 +0100

    iterate over blocks and add ids

--- gdpr-tree-processor.js
+++ gdpr-tree-processor.js
@@ -1,8 +1,25 @@
+var util = require('util')
+
+
+function processblocks(blocks) {
+      for (var i =0; i<blocks.length; i++) {
+          console.log(util.inspect(blocks[i]))
+          if(blocks[i].setId) {
+		blocks[i].setId("5");
+	}
+          if(blocks[i].hasBlocks && blocks[i].hasBlocks()){
+		processblocks(blocks[i].getBlocks())
+          }
+      }
+}
+
+
 module.exports = function (registry) {
   registry.treeProcessor(function () {
     var self = this
     self.process(function (doc) {
-      doc.getBlocks()[0] = self.createBlock(doc, 'paragraph', 'GDPR compliant :)')
+      blocks = doc.getBlocks()
+      processblocks(blocks)
       return doc
     })
   })
