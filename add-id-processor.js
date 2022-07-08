facf3129b0e8a45292ab18b7129776ab7027dd85
Paul Wright <pwright@redhat.com>
2022-07-08 12:03:04 +0100

    add classes

--- add-id-processor.js
+++ add-id-processor.js
@@ -1,28 +1,40 @@
-var util = require('util')
+var util = require("util");
 
-var counter = 0
+var counter = 0;
 
 function processblocks(blocks) {
-      for (var i =0; i<blocks.length; i++) {
-          if(blocks[i].getId && blocks[i].setId) {
-                if(!blocks[i].getId()) { // only add id if no existing id
-		      blocks[i].setId("comment_"+counter++);
-		}
-	}
-          if(blocks[i].hasBlocks && blocks[i].hasBlocks()){
-		processblocks(blocks[i].getBlocks())
-          }
-      }
-}
+  for (var i = 0; i < blocks.length; i++) {
+    if (blocks[i].getId && blocks[i].setId) {
+      if (!blocks[i].getId()) {
+        // only add id if no existing id
+        counter++;
+        blocks[i].setId("comment_" + counter);
+        //console.log(blocks[i]);
+        if (blocks[i].getText && blocks[i].setText) {
+          //console.log(blocks[i].getText());
+          text=blocks[i].getText();
+          blocks[i].addRole("comment_" + counter);
+        }
+        if (blocks[i].getSource) {
+          //console.log(blocks[i].getSource());
+          blocks[i].addRole("comment_" + counter);
+        }
 
+      }
+    }
+    if (blocks[i].hasBlocks && blocks[i].hasBlocks()) {
+      processblocks(blocks[i].getBlocks());
+    }
+  }
+}
 
 module.exports = function (registry) {
   registry.treeProcessor(function () {
-    var self = this
+    var self = this;
     self.process(function (doc) {
-      blocks = doc.getBlocks()
-      processblocks(blocks)
-      return doc
-    })
-  })
-}
+      blocks = doc.getBlocks();
+      processblocks(blocks);
+      return doc;
+    });
+  });
+};
